34694219e8add83d1cbc460b29b17ffabe01b6ef
Luqmaan Sulaiman <luqmaansu@gmail.com>
2024-02-19 00:06:31 +0800

    Update allowed hosts

--- _project/settings.py
+++ _project/settings.py
@@ -26,7 +26,10 @@ SECRET_KEY = local_settings.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "luqmaans.pythonanywhere.com",
+]
 
 
 # Application definition

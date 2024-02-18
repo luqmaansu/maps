a25f173fc5cd9be91968cd8c8f0b6f4266d753ce
Luqmaan Sulaiman <luqmaansu@gmail.com>
2024-02-18 13:01:20 +0800

    add url namespace maps

--- maps/urls.py
+++ maps/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 from . import api
 
+app_name = "maps"
+
 urlpatterns = [
     path("", views.MapView.as_view(), name="map"),
     path("api/locations/", api.LocationListAPIView.as_view(), name="api-location-list"),

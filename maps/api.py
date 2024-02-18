6ba3a05b7e095e23b796bf6a22e6480f1da97845
Luqmaan Sulaiman <luqmaansu@gmail.com>
2024-02-18 14:01:39 +0800

    API loaded data, cosmetics, data info, and refresh button

--- maps/api.py
+++ maps/api.py
@@ -2,6 +2,20 @@ from rest_framework.generics import ListAPIView
 from .models import Location
 from .serializers import LocationSerializer
 
+
 class LocationListAPIView(ListAPIView):
-    queryset = Location.objects.all()
     serializer_class = LocationSerializer
+
+    def get_queryset(self):
+        # import time
+
+        # time.sleep(1)
+        return Location.objects.all()
+
+    def list(self, request, *args, **kwargs):
+        # Return with info: Showing x of y locations
+        x = len(self.get_queryset())
+        y = Location.objects.count()
+        response = super().list(request, *args, **kwargs)
+        response["info"] = f"Showing {x} of {y} locations"
+        return response
